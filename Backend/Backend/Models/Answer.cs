5cc3ee47337829997415652c1e1dd814a99d1acc
Mihály Barczi <barczi.mihaly@gmail.com>
2019-02-27 17:17:09 +0100

    add quizquestion and answer entities

---
+++ Backend/Backend/Models/Answer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Answer
+    {
+        public long Id { get; set; }
+        public QuizQuestion QuestionId { get; set; }
+        public string Option { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
---
+++ Backend/Backend/Models/QuizQuestion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class QuizQuestion
+    {
+        public long Id { get; set; }
+        public string Question { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+}
