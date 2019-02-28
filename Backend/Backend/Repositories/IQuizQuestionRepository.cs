09e9b6105109e938c39c141da571a2d1d6436edc
Mihály Barczi <barczi.mihaly@gmail.com>
2019-02-28 15:03:35 +0100

    add quiz question repository

---
+++ Backend/Backend/Repositories/IQuizQuestionRepository.cs
@@ -0,0 +1,16 @@
+﻿using Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Repositories
+{
+    public interface IQuizQuestionRepository
+    {
+        IEnumerable<QuizQuestion> GetAll();
+        QuizQuestion GetById(long Id);
+        void Add(QuizQuestion question);
+        void DeleteById(long Id);
+    }
+}
---
+++ Backend/Backend/Repositories/QuizQuestionRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Data;
+using Backend.Models;
+
+namespace Backend.Repositories
+{
+    public class QuizQuestionRepository : IQuizQuestionRepository
+    {
+        private readonly QuizContext _quizContext;
+
+        public QuizQuestionRepository(QuizContext quizContext)
+        {
+            _quizContext = quizContext;
+        }
+
+        public void Add(QuizQuestion question)
+        {
+            _quizContext.Add(question);
+            _quizContext.SaveChanges();
+        }
+
+        public void DeleteById(long Id)
+        {
+            _quizContext.Remove(GetById(Id));
+            _quizContext.SaveChanges();
+        }
+
+        public IEnumerable<QuizQuestion> GetAll()
+        {
+            return _quizContext.QuizQuestions;
+        }
+
+        public QuizQuestion GetById(long Id)
+        {
+            return _quizContext.QuizQuestions.First(question => question.Id == Id);
+        }
+    }
+}
